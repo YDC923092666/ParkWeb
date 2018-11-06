006dc4fca305102d1b3ccdc1bccd93bf4773da9a
yangdongcheng <923092666@qq.com>
2018-11-06 11:06:11 +0800

    修改static目录

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,4 +125,4 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = "/home/yangdongcheng/parkingweb/app/static"
